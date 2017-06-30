992e4ff2583b28bbf9b6c0e18eaca3ca11405f6a
steven <steven@arxanfintech.com>
2017-06-30 10:05:15 +0800

    add problem description and one permutations related problem

---
+++ permutations/rich_man_fun.js
@@ -0,0 +1,16 @@
+function methodNum(n) {
+	if (n < 0) {
+		return 'number: ' + n + ' is a negative number';
+	} else if (n % 1 != 0) {
+		return 'number: ' + n + ' is not a Integer number';
+	} else {
+		if (n == 1) {
+			return 1;
+		}
+		if (n % 2 == 1) {
+			return Math.pow((n - 1), 2);
+		} else {
+			return 2 * methodNum(n - 1);
+		}
+	}
+}
